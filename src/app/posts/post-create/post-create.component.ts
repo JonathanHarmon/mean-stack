dcc21945524b016c44160826543dcb89f096bad5
Jonathan Harmon <jonathan@jonathanharmon.com>
2023-05-18 09:38:23 -0400

    Added image preview  and associated styling

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
 
@@ -26,7 +27,8 @@ export class PostCreateComponent implements OnInit {
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
-      content: new FormControl(null, {validators: [Validators.required]})
+      content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -51,6 +53,17 @@ export class PostCreateComponent implements OnInit {
       }
     });
   }
+
+onImagePicked(event: Event) {
+  const file = (event.target as HTMLInputElement).files[0];
+  this.form.patchValue({image: file});
+  this.form.get('image').updateValueAndValidity();
+  const reader = new FileReader();
+  reader.onload = () => {
+    this.imagePreview = reader.result as string;
+  };
+  reader.readAsDataURL(file);
+}
 
   onSavePost(){
     if (this.form.invalid) {
