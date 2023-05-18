e278510e34b2ad2cb8edc97c69d51dee7de6e535
Jonathan Harmon <jonathan@jonathanharmon.com>
2023-05-18 14:36:36 -0400

    updated app js to add path and images

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -15,12 +16,14 @@ mongoose.connect("mongodb+srv://jonathan:4e54kxlX54fK5eCp@cluster0.jrhfa.mongodb
 });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
